71ab6a9599d707836de40f616c23ba5c25a4d91c
Tyler Huntington <--get>
2018-04-07 20:46:47 -0400

    Start Viterbi class definition

---
+++ ViterbiClass.py
@@ -0,0 +1,18 @@
+"""
+ViterbiClass.py
+
+Definition of the Viterbi class for building a Hidden Markov
+Model for determining TMRCA from DNA sequence data.
+"""
+
+class Viberbi:
+
+    def __init__(fasta_file, param_file):
+
+        self.p_inits
+        self.p_trans
+        self.p_emits
+
+    def load_observed(self, fasta_file):
+
+    def load_params(self, param_file):
